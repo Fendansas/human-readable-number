268122115a36df0fd310b3956ed6ce68dd73721a
Sergey <fendansas@gmail.com>
2025-08-11 13:27:41 +0300

    feat: add js logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,68 @@
-module.exports = function toReadable(/* number */) {
-  throw new Error('Not implemented');
+module.exports = function toReadable(number) {
+  const ones = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+  const teens = [
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
+  const tens = [
+    '',
+    '',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+  ];
+
+  if (number < 10) {
+    return ones[number];
+  }
+  if (number >= 10 && number < 20) {
+    return teens[number - 10];
+  }
+  if (number < 100) {
+    const ten = Math.floor(number / 10);
+    const one = number % 10;
+    return one === 0 ? tens[ten] : `${tens[ten]} ${ones[one]}`;
+  }
+  if (number < 1000) {
+    const hundred = Math.floor(number / 100);
+    const rest = number % 100;
+    if (rest === 0) {
+      return `${ones[hundred]} hundred`;
+    }
+    return `${ones[hundred]} hundred ${toReadable(rest)}`;
+  }
+
+  const numberToString = Math.abs(number).toString();
+  const arr = numberToString.split('');
+  let reverse = '';
+  for (let i = arr.length - 1; i >= 0; i -= 1) {
+    reverse += arr[i];
+  }
+  const finalNum = parseInt(reverse, 10);
+
+  return Math.sign(number) * finalNum;
 };
